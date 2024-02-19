fc2ad63bd32ee322755bdfce192ab023c389115e
soyuul <syuul3139@gmail.com>
2024-02-19 11:46:14 +0900

    섹션 컴포넌트 구조화하기

--- src/App.js
+++ src/App.js
@@ -12,11 +12,16 @@ import Channel from './pages/Channel';
 import Video from './pages/Video';
 import Search from './pages/Search';
 import Not from './pages/Not';
+import Header from "./components/section/Header";
+import Main from "./components/section/Main";
+import Footer from "./components/section/Footer";
 
 
 function App() {
   return (
     <BrowserRouter>
+      <Header/>
+      <Main>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/today" element={<Today />} />
@@ -31,6 +36,8 @@ function App() {
         <Route path="/search/:searchID" element={<Search />} />
         <Route path="/*" element={<Not />} />
       </Routes>
+      </Main>
+      <Footer/>
     </BrowserRouter>
   );
 }
